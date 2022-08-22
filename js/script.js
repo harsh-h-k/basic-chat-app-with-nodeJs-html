4af6c09df508a97c3761988d795b5723ce25f7c2
harsh-h-k <dbm.mern.developer2@gmail.com>
2022-08-23 01:46:05 +0530

    added comments for better understanding

--- js/script.js
+++ js/script.js
@@ -1,17 +1,23 @@
+// Here we will link to the socket server, that here is running on port - 3300 
 const socket = io('http://localhost:3300')
 
+// We will get all elements that will be required while sending a message as well as showing the message 
 const form = document.getElementById('sendMessage-container')
 const messageInput = document.getElementById('messageInp')
 const messageContainer = document.querySelector('.container')
 
+// append Function - I have added this function to add message to div - messageContainer, where all messages will be shown 
 const append = (message, position) => {
+    // It will create a new div and message text  
     const messageElement = document.createElement('div')
     messageElement.innerHTML = message
+    // adding message class for styling and position class for left/right position 
     messageElement.classList.add('message')
     messageElement.classList.add(position)
     messageContainer.append(messageElement)
 }
 
+// When message is send from user, will listen event - submit 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     const message = messageInput.value
@@ -20,17 +26,22 @@ form.addEventListener('submit', (e) => {
     socket.emit('send', message)
 })
 
+// Prompting to get name of user 
 const userName = prompt("Enter Your Name To Join Chat") ;
+// Emitting new-user-joined event with that userName 
 socket.emit('new-user-joined', userName)
 
+// receiving data from user-joined event from server 
 socket.on('user-joined', name => {
     append(`${name} joined.`,'left')
 })
 
+// receive event to get message 
 socket.on('receive', data => {
     append(`${data.name} : ${data.message}`,'left')
 })
 
+// when any user left
 socket.on('leftChat', data => {
     append(`${data} left the chat`,'left')
 })
--- nodeServer/index.js
+++ nodeServer/index.js
@@ -1,18 +1,28 @@
+// Started Socket server at Port 3300 for local machine 
 const io = require("socket.io")((3300) || process.env.PORT)
 
+// to store details of all users that have joined the chat 
 const users = {}
 
+// Events that will be emitted or server will respond when the connection is successfully made 
 io.on('connection', socket => {
+    // When the event "new-user-joined" will be fired from frontEnd, It will give it's name, Here we can also add condition to limit the number of users in a chat
     socket.on('new-user-joined', name => {
+        // store details with their unique chat id
         users[socket.id] = name
+        // It will then broadcast to all users - event "user-joined" to inform about the user that has joined 
         socket.broadcast.emit('user-joined', name)
     })
 
+    // When a event - "send" is fired from frontEnd, means when anyone sends a message 
     socket.on('send', message => {
+        // We will broadcast a event - "receive" with message data to show to every joined user 
         socket.broadcast.emit('receive', {message : message, name : users[socket.id]})
     })
 
+    // When socket connection is lost , we get to know from frontEnd. It is a inbuild event of socket.io 
     socket.on('disconnect', message => {
+        // We will emit event - leftChat and broadcast to all to inform about the user that left 
         socket.broadcast.emit('leftChat', users[socket.id])
         delete users[socket.id]
     })
